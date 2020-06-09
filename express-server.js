2c51d708b4397fa4b9e6df8ba07f779462302a07
Adel Rashed <adel_rashed@outlook.com>
2020-06-09 18:03:07 +0000

    installed and required morgan package

--- express-server.js
+++ express-server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+
+
+
+
 
 /************************************************************************************* */
 const urlDatabase = {
