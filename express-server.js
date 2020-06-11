36e928cf94b147d48335540b6e3f6a7a850d3af5
Adel Rashed <adel_rashed@outlook.com>
2020-06-11 05:30:25 -0400

    modified handler for POST /urls/:shortURL such that only logged in users who own the url can update it

--- express-server.js
+++ express-server.js
@@ -194,11 +194,61 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //Post Request to update new short url
 app.post("/urls/:shortURL", (req, res) => {
+  const userID = req.cookies["user_id"];
   const shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect("/urls");
+  const url = urlDatabase[shortURL];
+  if (userID) {
+    // check if the user is logged in
+    if (url) {
+      // check if the url exists
+      if (url["userID"] === userID) {
+        // check if the current user is the owner/creator of the url
+
+        // updates the url
+
+        urlDatabase[shortURL][longURL] = req.body.longURL;
+
+        res.redirect("/urls");
+ 
+      } else {
+        // if the current user is not the creator of url
+        res.status(403);
+        res.send("Access Denied. The requested url belongs to another account");
+      }
+  
+    } else {
+      // the url does not exist in the database
+      res.status(400);
+      res.send("The url requested does not exist");
+    }
+      
+  } else {
+    // user is not logged in
+    res.status(403);
+    res.send('<p> Access Denied. Either <a href ="/login">Login</a> or <a href ="/register">Register</a> </p>');
+  }
+}
+
+
+
+
+
+
+
+
+
+
 }
 );
+
+
+
+
+
+
+
+
+
 
 
 
