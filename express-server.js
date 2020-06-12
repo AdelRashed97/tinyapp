07e1ebd18609e6e5ba520df967ce8c22f709aca3
Adel Rashed <adel_rashed@outlook.com>
2020-06-12 12:36:46 +0000

    refactored code to use isUserAllowed function to handle post requests for editing or deleting urls

--- express-server.js
+++ express-server.js
@@ -7,6 +7,7 @@ const {generateRandomString} = require("./generateRandomString");
 const {findUserByEmail} = require("./findUserByEmail");
 const {authenticateUser} = require("./authenticateUser");
 const {urlsForUser} = require("./urlsForUser");
+const {isUserAllowed} = require("./isUserAllowed");
 
 const app = express();
 const PORT = 8080; // default port 8080
@@ -231,40 +232,9 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const userID = req.session["user_id"];
   const shortURL = req.params.shortURL;
-  const url = urlDatabase[shortURL];
-  if (userID) {
-    // check if the user is logged in
-    if (url) {
-      // check if the url exists
-      if (url["userID"] === userID) {
-        // check if the current user is the owner/creator of the url
-
-        // deletes the url
-
-        delete urlDatabase[shortURL];
-        res.redirect("/urls");
- 
-      } else {
-        // if the current user is not the creator of url
-        res.status(403);
-        res.send("Access Denied. The requested url belongs to another account");
-      }
-  
-    } else {
-      // the url does not exist in the database
-      res.status(400);
-      res.send("The url requested does not exist");
-    }
-      
-  } else {
-    // user is not logged in
-    res.status(403);
-    res.send('<p> Access Denied. Either <a href ="/login">Login</a> or <a href ="/register">Register</a> </p>');
-  }
+  //const url = urlDatabase[shortURL];
+  isUserAllowed("delete",shortURL,userID,urlDatabase,req,res);
 }
-
-
-
 
 );
 
@@ -275,39 +245,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   const userID = req.session["user_id"];
   const shortURL = req.params.shortURL;
-  const url = urlDatabase[shortURL];
-  if (userID) {
-    // check if the user is logged in
-    if (url) {
-      // check if the url exists
-      if (url["userID"] === userID) {
-        // check if the current user is the owner/creator of the url
+  // const url = urlDatabase[shortURL];
+  isUserAllowed("update",shortURL,userID,urlDatabase,req,res);
 
-        // updates the url
-
-        urlDatabase[shortURL]["longURL"] = req.body.longURL;
-
-        res.redirect("/urls");
- 
-      } else {
-        // if the current user is not the creator of url
-        res.status(403);
-        res.send("Access Denied. The requested url belongs to another account");
-      }
-  
-    } else {
-      // the url does not exist in the database
-      res.status(400);
-      res.send("The url requested does not exist");
-    }
-      
-  } else {
-    // user is not logged in
-    res.status(403);
-    res.send('<p> Access Denied. Either <a href ="/login">Login</a> or <a href ="/register">Register</a> </p>');
-  }
 }
-
 );
 
 
